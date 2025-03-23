6fdce38e48c4bb84558db2e34a9c32ddb7483b5a
Anton Muradov <anton.muradov.d@gmail.com>
2025-03-23 23:23:11 +0300

    fixes

--- app/links/sql/get_redirect_count.sql
+++ app/links/sql/get_redirect_count.sql
@@ -1,3 +1,5 @@
 SELECT COUNT(*) AS total_redirects
 FROM redirects
-WHERE short_code = $1;
+WHERE short_code = $1
+    and short_code in (SELECT short_code FROM links WHERE owner_email = $2)
+GROUP BY short_code;
--- app/links/sql/update_link_by_short_code.sql
+++ app/links/sql/update_link_by_short_code.sql
@@ -1,4 +1,5 @@
 UPDATE links
 SET original_url = $1
 WHERE short_code = $2
+    and owner_email = $3
 RETURNING original_url, short_code;
